ac3373f03409b0b6925ea6c6f97eb1584e4aa2b9
Sadia Ansari <sadia765@gmail.com>
2020-10-13 12:45:58 -0500

    small change

--- Lines/lines.js
+++ Lines/lines.js
@@ -2,7 +2,6 @@ var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext("2d");
-var c = canvas.getContext("2d");
 
 window.onload = function () {
   window.addEventListener("click", randomLines);
